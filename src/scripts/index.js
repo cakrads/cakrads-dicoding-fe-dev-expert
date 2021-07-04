c1d91bbcc2864872c611448198aaef9f5492d3d4
Cakra Danu Sedayu <cakra.ds@gmail.com>
2021-07-05 01:02:37 +0700

    add restaurant list and footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
 import './drawer';
-import './render';
+import './render-resto';
 
---
+++ src/scripts/render-resto.js
@@ -0,0 +1,26 @@
+import DATA from '../DATA.json';
+
+const restaurantsElement = document.querySelector('#resto-data');
+
+function _renderRestaurants() {
+  const restaurants = { ...DATA };
+  const menuItem = restaurants.restaurants.map((item) => {
+    return (`<article class="resto-item">
+        <figure>
+          <img src="${item.pictureId}" alt="${item.name}">
+        </figure>
+        <div class="item-body">
+          <div class="title">
+            <h3><a href="#">${item.name}</a></h3>
+            <div class="rating">★ ${item.rating}</div>
+          </div>
+          <p>${item.description}</p>
+        </div>
+        <div class="badge bg-purple">${item.city}</div>
+      </article>`);
+  });
+
+  restaurantsElement.innerHTML = menuItem.join('');
+}
+
+_renderRestaurants();
