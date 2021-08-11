7cf13db85ab5e4c52b71a986d7ee05c6f260b282
Cakra Danu Sedayu <cakra.ds@gmail.com>
2021-08-11 09:16:51 +0700

    enhance keybord tab in drawer

--- src/scripts/drawer.js
+++ src/scripts/drawer.js
@@ -1,15 +1,20 @@
 
 const elBtnMenu = document.querySelector('#btn-menu');
 const elFullOverlay = document.querySelector('#full-overlay');
+const elDrawerMenu = document.querySelector('#drawer-menu');
 
 elBtnMenu.addEventListener('click', function (event) {
   showElFullOverlay(true);
   drawer.classList.toggle('open');
   event.stopPropagation();
-  if (drawer.classList.contains("open"))
+  if (drawer.classList.contains("open")) {
     elBtnMenu.innerHTML = '✕';
-  else
-    elBtnMenu.innerHTML = '☰';
+    setDrawerTabIndex(0);
+    return;
+  }
+
+  setDrawerTabIndex();
+  elBtnMenu.innerHTML = '☰';
 });
 
 elFullOverlay.addEventListener('click', function (event) {
@@ -20,4 +25,11 @@ elFullOverlay.addEventListener('click', function (event) {
 
 function showElFullOverlay(show = false) {
   elFullOverlay.classList.toggle('open');
+}
+
+function setDrawerTabIndex(value = -1) {
+  const listitems = elDrawerMenu.getElementsByTagName("a");
+  for (i = 0; i < listitems.length; i++) {
+    listitems[i].setAttribute("tabindex", value);
+  }
 }
