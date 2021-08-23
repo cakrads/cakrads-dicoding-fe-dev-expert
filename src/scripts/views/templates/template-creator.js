cafb2ba5f60d0aa8d59143b72855eb2b0ed1fa40
Cakra Danu Sedayu <cakra.ds@gmail.com>
2021-08-23 22:55:19 +0700

    [rpa] refactor template home and add template detail (not ready to use)

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -2,9 +2,10 @@ import CONFIG from '../../globals/config';
 
 const CardRestaurant = (item) => `
   <article class="resto-item">
-    <figure>
-      <img src="${`${CONFIG.BASE_IMAGE_URL}medium/${item.pictureId}`}" alt="${item.name}">
-    </figure>
+    <picture>
+      <source media="(min-width:600px)" srcset="${`${CONFIG.BASE_IMAGE_URL}medium/${item.pictureId}`}">
+      <img src="${`${CONFIG.BASE_IMAGE_URL}small/${item.pictureId}`}" alt="${item.name}">
+    </picture>
     <div class="item-body">
       <div class="title">
         <a href="#" tabindex="0" title="Restauran ${item.name}">
